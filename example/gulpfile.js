fc3b6f0deab2b30e3e7de7e8cfd34f12b4af2d72
Kevin Moot <kmoot@nerdery.com>
2015-04-19 19:12:30 -0500

    Added example implementation Allowed supplying of buster value via gulp option

---
+++ example/gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var bust = require('gulp-css-asset-cachebuster');
+
+gulp.task('default', function() {
+    return gulp.src('lib/styles/screen.css')
+        .pipe(bust({
+            // Will append ?v=1.2.3 as a query param for every url()
+            buster: '1.2.3'
+        }))
+        .pipe(gulp.dest('dist/'))
+});
